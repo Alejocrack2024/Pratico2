6e035c07f633047fe6e42b22fc5acec197bde0b6
gigan <gigantosaurio420@gmail.com>
2025-07-28 07:55:36 -0300

    Segundo commit: agrego todos los archivos

---
+++ crud/oficina/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Oficina
+
+class OficinaListView(ListView):
+    model = Oficina
+    template_name = 'oficina/lista.html'
+    context_object_name = 'oficinas'
+
+# Create your views here.
